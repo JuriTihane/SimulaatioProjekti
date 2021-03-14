a04a2d6b1c8b9150d82c7b8139f1f0c80e39eb94
juritihane <juri.tihane@gmail.com>
2021-03-14 20:13:52 +0200

    JavaDoc documentation for framework directory

--- src/main/java/simu/framework/Tapahtumalista.java
+++ src/main/java/simu/framework/Tapahtumalista.java
@@ -1,30 +1,39 @@
 package simu.framework;
 
 import java.util.PriorityQueue;
-import simu.framework.*;
-import simu.model.*;
 
-
+/**
+ * Tapahtumalista luokka ylläpitää tapahtumalistan parametrit, muuttujat ja tietorakennen
+ */
 public class Tapahtumalista {
 
 	private PriorityQueue<Tapahtuma> lista = new PriorityQueue<Tapahtuma>();
 
+	/**
+	 * Tapahtumalista konstruktori
+	 */
 	public Tapahtumalista(){
 	}
-	
+
+	/**
+	 * @return Palauttaa ensimmäisen Tapahtuman listalta, jonka poistetaan
+	 */
 	public Tapahtuma poista(){
 		//Trace.out(Trace.Level.INFO,"Tapahtumalistasta poisto " + lista.peek());
 		return lista.remove();
 	}
 
-	public void lisaaBussiTapahtumia (Tapahtuma t){
-		lisaa(t);
-	}
-	
+	/**
+	 * Lisää tapahtumalistaan uuden Tapahtuman t
+	 * @param t Tapahtuma olio
+	 */
 	public void lisaa(Tapahtuma t){
 		lista.add(t);
 	}
-	
+
+	/**
+	 * @return Palauttaa seuraavan tapahtumalistan tapahtuman ajan
+	 */
 	public double getSeuraavanAika(){
 		return lista.peek().getAika();
 	}
--- src/main/java/simu/framework/Trace.java
+++ src/main/java/simu/framework/Trace.java
@@ -1,15 +1,27 @@
 package simu.framework;
 
+/**
+ * Trace luokka
+ */
 public class Trace {
 
 	public enum Level{INFO, WAR, ERR}
-	
+
 	private static Level traceLevel;
-	
+
+	/**
+	 * Asettaa trace levelin
+	 * @param lvl e.g. INFO vai WAR vai ERR trace level
+	 */
 	public static void setTraceLevel(Level lvl){
 		traceLevel = lvl;
 	}
 
+	/**
+	 * Ei vielä käytössä
+	 * @param lvl
+	 * @param txt
+	 */
 	public static void out(Level lvl, String txt) {
 		if (lvl.ordinal() >= traceLevel.ordinal()) {
 			//System.out.println(txt);
